0515fb015b1f354ad92d320f2ba5fc74511aacf4
Debo <dwdeante0726@gmail.com>
2026-06-01 21:06:51 -0700

    Adding user input logic and UI for inputting. :p

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ function App() {
   const [students, setStudents] = useState([]);
   const [input, setInput] = useState("");
 
+    function addStudent() {
+      if (input.trim()) return;
+
+      setStudents([...students, input]);
+      setInput("");
+    }
   return (
     <div>
       <h1>Seating Chart Generator</h1>
 
-      {/* show all students */}
-      {students.map(student => (
+      {/* INPUT BOX */}
+      <input
+        value={input}
+        onChange={e => setInput(e.target.value)}
+        placeholder="Enter student name"
+      />
+      
+      <button onClick={addStudent}>
+        Add Student
+      </button>
+      
+      // STUDENT LIST
+      {students.map((student, index) => (
         <div key={student}>
           {student}
         </div>
